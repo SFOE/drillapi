9168c99f86ecfa959e912a6af424d48621e73fc0
rochette <olivier.monod@gmail.com>
2025-12-17 11:12:47 +0100

    fix test import

--- tests/test_configuration_structure.py
+++ tests/test_configuration_structure.py
@@ -1,6 +1,6 @@
 from typing import List, Optional, Union
 from pydantic import BaseModel, HttpUrl, field_validator, ValidationError, conlist
-from drillapi.cantons_configuration import CANTONS
+from drillapi.cantons_configuration.cantons import CANTONS
 
 
 class PropertyValue(BaseModel):
