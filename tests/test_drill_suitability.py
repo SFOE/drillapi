1a5f1eb98f718e7b6f51d0b7fbbfeea7ce49283b
Olivier Monod <olivier.monod@gmail.com>
2026-01-08 14:29:46 +0000

    add test for esri rest json response FR

--- tests/test_drill_suitability.py
+++ tests/test_drill_suitability.py
@@ -49,5 +49,51 @@ def test_drill_category_wms_gml(client):
     assert payload["coord_x"] == coord_x
     assert payload["coord_y"] == coord_y
     assert payload["ground_category"]
+    assert payload["ground_category"]["harmonized_value"] == 1
+    assert "full_url" in payload["result_detail"]
+    assert payload["result_detail"]["detail"] is None
+
+
+@respx.mock
+def test_drill_category_esri_json(client):
+    coord_x = 2582124
+    coord_y = 1164966
+
+    with open("tests/data/geoadmin/canton_identify_fr.json", "rb") as f:
+        canton_json = f.read()
+
+    respx.get(
+        "https://api3.geo.admin.ch/rest/services/ech/MapServer/identify", params=None
+    ).mock(
+        return_value=httpx.Response(
+            200,
+            content=canton_json,
+            headers={"Content-Type": "application/json"},
+        )
+    )
+
+    with open("tests/data/esri/identify_fr.json", "rb") as f:
+        json = f.read()
+
+    respx.get(
+        "https://map.geo.fr.ch/arcgis/rest/services/PortailCarto/Theme_environnement/MapServer/17/query",
+        params=None,
+    ).mock(
+        return_value=httpx.Response(
+            200,
+            content=json,
+            headers={"Content-Type": "application/json"},
+        )
+    )
+
+    response = client.get(f"/v1/drill-category/{coord_x}/{coord_y}")
+
+    assert response.status_code == 200
+    payload = response.json()
+    assert payload["status"] == "success"
+    assert payload["coord_x"] == coord_x
+    assert payload["coord_y"] == coord_y
+    assert payload["ground_category"]
+    assert payload["ground_category"]["harmonized_value"] == 1
     assert "full_url" in payload["result_detail"]
     assert payload["result_detail"]["detail"] is None
