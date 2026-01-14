3cb126a446d6199fe6953d6701e4a4cf11758f2b
monodo <olivier.monod@gmail.com>
2026-01-14 19:55:39 +0100

    Add respx tests (#94)
    * create mocks for WMS and ESRI rest reponses
    * test drill-category for this 2 cases

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,24 @@
 import pytest
-from drillapi.config import Settings, settings as global_settings
+from fastapi.testclient import TestClient
+from drillapi.config import settings, Settings
+from drillapi.app import app
 
 
 @pytest.fixture(autouse=True, scope="session")
 def override_global_settings():
-    """
-    Automatically override settings for all tests.
-    This fixture is applied to all tests without needing to include it explicitly.
-    """
-    global global_settings
-    global_settings = Settings(
+    test_settings = Settings(
         RATE_LIMIT="100/min",
         ALLOWED_IPS=["127.0.0.1"],
         ALLOWED_ORIGINS=["*"],
         ENVIRONMENT="TEST",
     )
+
+    for key, value in test_settings.model_dump().items():
+        setattr(settings, key, value)
+
+    yield
+
+
+@pytest.fixture(scope="session")
+def client():
+    return TestClient(app)
---
+++ tests/test_drill_suitability.py
@@ -0,0 +1,99 @@
+import pytest
+import respx
+import httpx
+from fastapi.testclient import TestClient
+from drillapi.app import app
+
+
+@pytest.fixture
+def client():
+
+    with TestClient(app) as c:
+        yield c
+
+
+@respx.mock
+def test_drill_category_wms_gml(client):
+    coord_x = 2574738
+    coord_y = 1249285
+
+    with open("tests/data/geoadmin/canton_identify_ju.json", "rb") as f:
+        canton_json = f.read()
+
+    respx.get(
+        "https://api3.geo.admin.ch/rest/services/ech/MapServer/identify", params=None
+    ).mock(
+        return_value=httpx.Response(
+            200,
+            content=canton_json,
+            headers={"Content-Type": "application/json"},
+        )
+    )
+
+    with open("tests/data/wms/getfeatureinfo_ju.gml", "rb") as f:
+        gml = f.read()
+
+    respx.get("https://geoservices.jura.ch/wms", params=None).mock(
+        return_value=httpx.Response(
+            200,
+            content=gml,
+            headers={"Content-Type": "application/vnd.ogc.gml"},
+        )
+    )
+
+    response = client.get(f"/v1/drill-category/{coord_x}/{coord_y}")
+
+    assert response.status_code == 200
+    payload = response.json()
+    assert payload["status"] == "success"
+    assert payload["coord_x"] == coord_x
+    assert payload["coord_y"] == coord_y
+    assert payload["ground_category"]
+    assert payload["ground_category"]["harmonized_value"] == 1
+    assert "full_url" in payload["result_detail"]
+    assert payload["result_detail"]["detail"] is None
+
+
+@respx.mock
+def test_drill_category_esri_json(client):
+    coord_x = 2582124
+    coord_y = 1164966
+
+    with open("tests/data/geoadmin/canton_identify_fr.json", "rb") as f:
+        canton_json = f.read()
+
+    respx.get(
+        "https://api3.geo.admin.ch/rest/services/ech/MapServer/identify", params=None
+    ).mock(
+        return_value=httpx.Response(
+            200,
+            content=canton_json,
+            headers={"Content-Type": "application/json"},
+        )
+    )
+
+    with open("tests/data/esri/identify_fr.json", "rb") as f:
+        json = f.read()
+
+    respx.get(
+        "https://map.geo.fr.ch/arcgis/rest/services/PortailCarto/Theme_environnement/MapServer/17/query",
+        params=None,
+    ).mock(
+        return_value=httpx.Response(
+            200,
+            content=json,
+            headers={"Content-Type": "application/json"},
+        )
+    )
+
+    response = client.get(f"/v1/drill-category/{coord_x}/{coord_y}")
+
+    assert response.status_code == 200
+    payload = response.json()
+    assert payload["status"] == "success"
+    assert payload["coord_x"] == coord_x
+    assert payload["coord_y"] == coord_y
+    assert payload["ground_category"]
+    assert payload["ground_category"]["harmonized_value"] == 1
+    assert "full_url" in payload["result_detail"]
+    assert payload["result_detail"]["detail"] is None
